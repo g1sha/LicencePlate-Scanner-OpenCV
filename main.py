4f55fcefefd5e5777bd49557717a986278f206ea
g1sha <emir.ibrisimovic2@gmail.com>
2021-04-12 15:17:15 +0200

    Detekcija, greške u detekciji

--- main.py
+++ main.py
@@ -3,17 +3,49 @@ import numpy as np
 import imutils
 import pytesseract as tess
 
-def ucitajSliku(img_path):
-    img = cv.imread(img_path)
-    gscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # Grayscale ubacene slike 
-    cv.imshow("se1", gscale)
 
-    gscale = cv.bilateralFilter(gscale, 13, 30, 30) #Blurovanje nepotrebnih dijelova na slici
-    edge = cv.Canny(gscale, 30, 300) # Otkrivanje ivica
-    cv.imshow("se2", edge)
-    cv.imshow("se", gscale)
 
-ucitajSliku("images/images.jpg") ## Prva slika
-#ucitajSliku("images/tabla.jpg") ## Druga slika 
+img = cv.imread('images/golf2.jpg')
+img = cv.resize(img, (500,300) )
+
+cv.imshow('Full Original',img)
+
+
+blnkSc = None
+
+gscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
+gscale = cv.bilateralFilter(gscale, 13, 15, 15) 
+
+cv.imshow('BNW Original',gscale)
+
+ivice = cv.Canny(gscale, 30, 200) 
+
+cts = cv.findContours(ivice.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cts = imutils.grab_contours(cts)
+cts = sorted(cts, key = cv.contourArea, reverse = True)[:10]
+
+for x in cts:
+    kriva = cv.arcLength(x, True)
+    poligon = cv.approxPolyDP(x, 0.020 * kriva, True)
+
+    if len(poligon) == 4:
+        blnkSc = poligon
+        break
+
+cv.drawContours(img, [blnkSc], -1, (0, 0, 255), 3)
+
+mask = np.zeros(gscale.shape,np.uint8)
+kopija = cv.drawContours(mask,[blnkSc],0,255,-1,)
+kopija = cv.bitwise_and(img,img,mask=mask)
+
+(x, y) = np.where(mask == 255)
+(topx, topy) = (np.min(x), np.min(y))
+(bottomx, bottomy) = (np.max(x), np.max(y))
+tablica = gscale[topx:bottomx+1, topy:bottomy+1]
+
+img = cv.resize(img,(500,300))
+tablica = cv.resize(tablica,(400,200))
+cv.imshow('Tablica',tablica)
+cv.imshow('Full Original detektovana',img)
 
 cv.waitKey(0)

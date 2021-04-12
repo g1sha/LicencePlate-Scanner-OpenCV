1fcf453338c455f8bd31be9165d3892919a1046d
g1sha <emir.ibrisimovic2@gmail.com>
2021-04-12 16:59:24 +0200

    Crop cetvrtine dodan, program podijenjen na funkcije

--- main.py
+++ main.py
@@ -4,48 +4,59 @@ import imutils
 import pytesseract as tess
 
 
+def detektujTablicu(img):
+    img = cv.resize(img, (500,300) )
 
-img = cv.imread('images/golf2.jpg')
-img = cv.resize(img, (500,300) )
-
-cv.imshow('Full Original',img)
+    cv.imshow('Full Original',img)
 
 
-blnkSc = None
+    blnkSc = None
 
-gscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
-gscale = cv.bilateralFilter(gscale, 13, 15, 15) 
+    gscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
+    gscale = cv.bilateralFilter(gscale, 13, 15, 15) 
 
-cv.imshow('BNW Original',gscale)
+    cv.imshow('BNW Original',gscale)
 
-ivice = cv.Canny(gscale, 30, 200) 
+    ivice = cv.Canny(gscale, 30, 200) 
 
-cts = cv.findContours(ivice.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-cts = imutils.grab_contours(cts)
-cts = sorted(cts, key = cv.contourArea, reverse = True)[:10]
+    cts = cv.findContours(ivice.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cts = imutils.grab_contours(cts)
+    cts = sorted(cts, key = cv.contourArea, reverse = True)[:10]
 
-for x in cts:
-    kriva = cv.arcLength(x, True)
-    poligon = cv.approxPolyDP(x, 0.020 * kriva, True)
+    for x in cts:
+        kriva = cv.arcLength(x, True)
+        poligon = cv.approxPolyDP(x, 0.020 * kriva, True)
 
-    if len(poligon) == 4:
-        blnkSc = poligon
-        break
+        if len(poligon) == 4:
+            blnkSc = poligon
+            break
 
-cv.drawContours(img, [blnkSc], -1, (0, 0, 255), 3)
+    cv.drawContours(img, [blnkSc], -1, (0, 0, 255), 3)
 
-mask = np.zeros(gscale.shape,np.uint8)
-kopija = cv.drawContours(mask,[blnkSc],0,255,-1,)
-kopija = cv.bitwise_and(img,img,mask=mask)
+    mask = np.zeros(gscale.shape,np.uint8)
+    kopija = cv.drawContours(mask,[blnkSc],0,255,-1,)
+    kopija = cv.bitwise_and(img,img,mask=mask)
 
-(x, y) = np.where(mask == 255)
-(topx, topy) = (np.min(x), np.min(y))
-(bottomx, bottomy) = (np.max(x), np.max(y))
-tablica = gscale[topx:bottomx+1, topy:bottomy+1]
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    tablica = gscale[topx:bottomx+1, topy:bottomy+1]
 
-img = cv.resize(img,(500,300))
-tablica = cv.resize(tablica,(400,200))
-cv.imshow('Tablica',tablica)
-cv.imshow('Full Original detektovana',img)
+    img = cv.resize(img,(500,300))
+    tablica = cv.resize(tablica,(400,200))
+    cv.imshow('Tablica',tablica)
+    cv.imshow('Full Original detektovana',img)
 
+def cropCetvrtina(img):
+    cropCV_1 = img[0:(img.shape[0]//2), 0:(img.shape[1]//2)]
+    cv.rectangle(img,((img.shape[1]//2),0), ((img.shape[1]), img.shape[0]//2), (0,255,0),thickness=2)
+    cv.putText(img, "Polje za crop 1/4 slike", ((img.shape[1]//2)-110, (img.shape[0]//2)-10), cv.FONT_HERSHEY_PLAIN, 0.5, (0,255,0), 1)
+    cv.imshow("crop", cropCV_1)
+
+img = cv.imread('images/golf.jpg') ## UCITAVANJE SLIKE !
+## SPLIT SLIKU U DIJELOVE
+cropCetvrtina(img)
+
+
+detektujTablicu(img)
 cv.waitKey(0)
